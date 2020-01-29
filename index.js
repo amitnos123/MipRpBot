a4a36cdc16846cf534d2f804481075b8830f852e
amitnos123 <sami.nosatzki@gmail.com>
2020-01-30 00:05:52 +0200

    Finished first version of main file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const log_writer = require('log_writer');
+const constants = require('consts');
 const path = require('path');
+const fs = require('fs');
 
-const lw = new log_writer(path.join(__dirname, 'log'));
-//lw.logSplit();
+const lw = new log_writer(constants.LOG_DIRECTORY_PATH);
 
 const Discord = require('discord.js');
 lw.logMessage('load', 'Loaded discord.js');
@@ -10,42 +11,72 @@ lw.logMessage('load', 'Loaded discord.js');
 lw.logMessage('start', 'Creating client');
 const client = new Discord.Client();
 
+lw.logMessage('start', 'Syncing Server Commands');
+client.serverCommand = new Discord.Collection();
+const serverCommandFiles = fs.readdirSync(constants.SERVER_COMMANDS_DIRECTORY_PATH).filter(file => file.endsWith('.js'));
+
+for (const file of serverCommandFiles) {
+  const command = require(constants.SERVER_COMMANDS_DIRECTORY_PATH + `\\${file}`);
+  lw.logMessage('start', '---' + command.name + ' was synced');
+  client.serverCommand.set(command.name, command);
+}
+
+lw.logMessage('start', 'Syncing Personal Commands');
+client.personalCommand = new Discord.Collection();
+const personalCommandFiles = fs.readdirSync(constants.PERSONAL_COMMANDS_DIRECTORY_PATH).filter(file => file.endsWith('.js'));
+
+for (const file of personalCommandFiles) {
+  const command = require(constants.PERSONAL_COMMANDS_DIRECTORY_PATH + `\\${file}`);
+  lw.logMessage('start', '---' + command.name + ' was synced');
+  client.personalCommand.set(command.name, command);
+}
+
 const { prefix, token } = require('./config.json');
 
+lw.logMessage('start', 'Connecting to server');
 client.login(token);
+lw.logMessage('start', 'Connected to server');
 
 client.on('ready', function() {
   lw.logSplit();
   lw.logMessage('start', 'Bot is ready');
-  lw.logMessage('start', path.join(__dirname, 'data'));
 });
-/*
-
-logMessage('load', 'Loading config');
-const { prefix, token } = require('./config.json');
-
-
-
-const fs = require('fs');
-logMessage('start', 'Loaded fs');
 
 client.on('message', message => {
   if (message.author.bot) return;
-
-  if (!message.content.startsWith(prefix)) {
-    nonCommandMessage(message);
-    return;
-  }
+  
+  if (!message.content.startsWith(prefix)) return;
 
   try {
-    logMessage('info', 'On message Start');
+    lw.logMessage('info', 'On message Start');
 
     const args = message.content.slice(prefix.length).split(' ');
-	
+    const command = args.shift();
+    lw.logMessage('debug', `${command} was called, with arguments: ${args}`);
 
-    logMessage('info', 'On message End');
+    if(message.channel.type === constants.CHANNELS_TYPES_DM) {
+      if (!client.personalCommand.has(command)) {
+        error404(message);
+        return;
+      }
+
+      client.personalCommand.get(command).execute(client, message, args);
+    } else if(message.channel.type === constants.CHANNELS_TYPES_TEXT) {
+      if (!client.serverCommand.has(command)) {
+        error404(message);
+        return;
+      }
+
+      client.serverCommand.get(command).execute(client, message, args);
+    }
+
+    lw.logMessage('info', 'On message End');
   }
   catch (err) {
-    logMessage('error', err);
+    lw.logMessage('error', err);
   }
-});*/
+});
+
+function error404(message) {
+  message.channel.send('Error 404: Function doesn\'t exist');
+}
