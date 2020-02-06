f84864c6a0ae3b5b67e75edd3dbf05b077b8998e
amitnos123 <sami.nosatzki@gmail.com>
2020-02-06 21:51:20 +0200

    Cleaned some test functions

--- personal_commands/blacklistCommand.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-module.exports = {
-  name: path.basename(__filename).split('.').slice(0, -1).join('.'),
-  description: '',
-  args: true,
-  execute(client, message, args) {
-    //CODE
-  },
-};
