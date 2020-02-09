f3e549932e8ba67a1c07959fa03c77d09cc2555b
amitnos123 <sami.nosatzki@gmail.com>
2020-02-09 11:23:25 +0200

    Removed id from module

--- server_commands/test_server_command2.js
+++ server_commands/test_server_command2.js
@@ -3,7 +3,6 @@ module.exports = {
   name: path.basename(__filename).split('.').slice(0, -1).join('.'),
   description: 'Information about the arguments provided.',
   args: true,
-  id: "text2",
   execute(client, message, args) {
     message.channel.send(`SERVER TEXT MESSAGE 2`);
     const commandsIdManager = require('commands_id_manager');
