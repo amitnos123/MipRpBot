b4e7f03f916dea9248c4b051d618d16490ed36a7
amitnos123 <sami.nosatzki@gmail.com>
2020-02-09 10:29:35 +0200

    Created 2 commands files for the future

---
+++ personal_commands/removeStat.js
@@ -0,0 +1,10 @@
+const path = require('path');
+module.exports = {
+    name: path.basename(__filename).split('.').slice(0, -1).join('.'),
+    description: '',
+    args: true,
+    execute(client, message, args) {
+        //CODE
+        message.channel.send('Implement');
+    },
+};
