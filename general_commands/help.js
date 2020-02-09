f9803d5bf4622575c739710c71fdef09b843e482
amitnos123 <sami.nosatzki@gmail.com>
2020-02-09 19:58:38 +0200

    Made help command

--- general_commands/help.js
+++ general_commands/help.js
@@ -4,7 +4,40 @@ module.exports = {
     description: 'Give information on how to use the command and which options does it support.',
     args: true,
     execute(client, message, args) {
-        //CODE
-        message.channel.send('Implement');
+        if (args.length === 0) {
+            message.channel.send('No command name was given.');
+        } else {
+            for (let index = 0; index < args.length; index++) {
+                const commandName = args[index];
+                if (client.generalCommand.has(commandName)) { // Type General
+                    message.channel.send(create_help_command_embed_message(commandName, client.generalCommand.get(commandName), '#297780'));
+                } else if (client.personalCommand.has(commandName)) { // Type General
+                    message.channel.send(create_help_command_embed_message(commandName, client.personalCommand.get(commandName), '#297780'));
+                } else if (client.serverCommand.has(commandName)) { // Type General
+                    message.channel.send(create_help_command_embed_message(commandName, client.serverCommand.get(commandName), '#297780'));
+                }
+            }
+        }
     },
 };
+
+/**
+ * Create a RichEmbed for the commands given
+ * @param {module} command - The command's module
+ * @param {string} title - Title if the RichEmbed
+ * @param {string} color - Color of the RichEmbed
+ * @returns {discord.RichEmbed} - The RichEmbed which was created and defined from the given commands, title, color
+ */
+function create_help_command_embed_message(title, command, color) {
+    const discord = require('discord.js');
+    const embed = new discord.RichEmbed()
+        .setColor(color)
+        .setTitle(title);
+
+    if (command.help !== undefined) {
+        embed.setDescription(command.help);
+    } else {
+        embed.setDescription('Help isn\'t defined of the command.');
+    }
+    return embed;
+}
