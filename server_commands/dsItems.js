da0b52250eb976c7a919c982c07c70b690c2f455
amitnos123 <sami.nosatzki@gmail.com>
2020-02-13 00:37:08 +0200

    Created and tested grItems and dsItems

--- server_commands/dsItems.js
+++ server_commands/dsItems.js
@@ -1,9 +1,68 @@
 const path = require('path');
+const constants = require('consts');
+const log_writer = require('log_writer');
+const lw = new log_writer(constants.LOG_DIRECTORY_PATH);
 module.exports = {
     name: path.basename(__filename).split('.').slice(0, -1).join('.'),
     description: 'Destory items which character has.',
     args: true,
     execute(client, message, args) {
-        message.channel.send('Implement');
+        if (args.length === 0) {
+            message.channel.send('No Arguments were given.');
+        } else if (args.length < 3) {
+            message.channel.send('Not enought Arguments were given.');
+        } else if (args.length % 2 === 0) {
+            message.channel.send('Not all items got quantity to transfer.');
+        } else {
+            const toCharacterName = args.shift(); // Take toCharName from the args and remove it
+            // After the shift twice, args only holds items' name and quantity
+
+            const charactersManager = require('characters_manager');
+            const charMan = new charactersManager();
+
+            const toCharacter = charMan.get(toCharacterName);
+
+            // If toCharacter doesn't have an inventory, then create an empty one
+            if (toCharacter.inventory === undefined) {
+                toCharacter.inventory = {};
+            }
+
+            let flagDestroyedItem = false;
+
+            for (let index = 0; index < args.length; index += 2) {
+                // Jump by 2, because the on index it's the item name and index + 1 is the quantity
+                const itemName = args[index];
+                const quantity = parseInt(args[index + 1]);
+
+                if (quantity > 0) {
+                    // Add item toCharacter
+                    if (toCharacter.inventory.hasOwnProperty(itemName)) {
+                        if (quantity > parseInt(toCharacter.inventory[itemName])) {
+                            message.channel.send(`Didn't destroy ${itemName}. Character ${toCharacterName} has less quantity then the requried amount to be destroyed`);
+                        } else {
+                            // If has item, then add to the item's quantity
+                            toCharacter.inventory[itemName] = (parseInt(toCharacter.inventory[itemName]) - quantity).toString();
+                            if ((parseInt(toCharacter.inventory[itemName])) === 0) {
+                                delete toCharacter.inventory[itemName];
+                            }
+                            flagDestroyedItem = true;
+                        }
+                    } else {
+                        message.channel.send(`Didn't destroy ${itemName}. Character ${toCharacterName} doesn't has ${itemName}.`);
+                    }
+                }
+            }
+
+            charMan.update(toCharacterName, 'inventory', toCharacter.inventory);
+            lw.log_message('debug', `${toCharacterName} new inventory ${JSON.stringify(toCharacter.inventory, undefined, 2)}.`);
+
+            if (flagDestroyedItem) {
+                if (args.length > 2) {
+                    message.channel.send('Items were destroyed.');
+                } else {
+                    message.channel.send('Item was destroyed.');
+                }
+            }
+        }
     },
 };
--- server_commands/grItems.js
+++ server_commands/grItems.js
@@ -1,9 +1,57 @@
 const path = require('path');
+const constants = require('consts');
+const log_writer = require('log_writer');
+const lw = new log_writer(constants.LOG_DIRECTORY_PATH);
 module.exports = {
     name: path.basename(__filename).split('.').slice(0, -1).join('.'),
     description: 'Grand items for a character.',
     args: true,
     execute(client, message, args) {
-        message.channel.send('Implement');
+        if (args.length === 0) {
+            message.channel.send('No Arguments were given.');
+        } else if (args.length < 3) {
+            message.channel.send('Not enought Arguments were given.');
+        } else if (args.length % 2 === 0) {
+            message.channel.send('Not all items got quantity to transfer.');
+        } else {
+            const toCharacterName = args.shift(); // Take toCharName from the args and remove it
+            // After the shift twice, args only holds items' name and quantity
+
+            const charactersManager = require('characters_manager');
+            const charMan = new charactersManager();
+
+            const toCharacter = charMan.get(toCharacterName);
+
+            // If toCharacter doesn't have an inventory, then create an empty one
+            if (toCharacter.inventory === undefined) {
+                toCharacter.inventory = {};
+            }
+
+            for (let index = 0; index < args.length; index += 2) {
+                // Jump by 2, because the on index it's the item name and index + 1 is the quantity
+                const itemName = args[index];
+                const quantity = parseInt(args[index + 1]);
+
+                if (quantity > 0) {
+                    // Add item toCharacter
+                    if (toCharacter.inventory.hasOwnProperty(itemName)) {
+                        // If has item, then add to the item's quantity
+                        toCharacter.inventory[itemName] = (parseInt(toCharacter.inventory[itemName]) + quantity).toString();
+                    } else {
+                        // If doesn't has item, then add to the item in the inventory
+                        toCharacter.inventory[itemName] = quantity.toString();
+                    }
+                }
+            }
+
+            charMan.update(toCharacterName, 'inventory', toCharacter.inventory);
+            lw.log_message('debug', `${toCharacterName} new inventory ${JSON.stringify(toCharacter.inventory, undefined, 2)}.`);
+
+            if (args.length > 2) {
+                message.channel.send('Items were granted.');
+            } else {
+                message.channel.send('Item was granted.');
+            }
+        }
     },
 };
